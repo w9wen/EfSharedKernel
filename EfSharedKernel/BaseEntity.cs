100e09630311e6d6d80ce45f607d16108be6c03d
William (PC) <william.wen@gmail.com>
2020-12-23 06:45:10 +0800

    [DB] Add the common columns.

--- EfSharedKernel/BaseEntity.cs
+++ EfSharedKernel/BaseEntity.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace w9wen.EfSharedKernel
 {
@@ -8,5 +11,63 @@ namespace w9wen.EfSharedKernel
         public int Id { get; set; }
 
         public List<BaseDomainEvent> Events = new List<BaseDomainEvent>();
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        [DisplayName("建立時間")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        [Editable(false)]
+        [UIHint("DateTime")]
+        public DateTime CreatedDateTime { get; set; }
+
+        /// <summary>
+        /// 建立者
+        /// </summary>
+        [DisplayName("建立者")]
+        [Editable(false)]
+        [StringLength(50)]
+        public string CreatedUser { get; set; }
+
+        /// <summary>
+        /// 更新次數
+        /// </summary>
+        [DisplayName("更新次數")]
+        [Editable(false)]
+        public byte UpdatedTimes { get; set; }
+
+        /// <summary>
+        /// 更新時間
+        /// </summary>
+        [DisplayName("更新時間")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        [Editable(false)]
+        [UIHint("DateTime")]
+        public DateTime UpdatedDateTime { get; set; }
+
+        /// <summary>
+        /// 更新者
+        /// </summary>
+        [DisplayName("更新者")]
+        [Editable(false)]
+        [StringLength(50)]
+        public string UpdatedUser { get; set; }
+
+        /// <summary>
+        /// 生效標記
+        /// </summary>
+        [DisplayName("生效標記")]
+        [Editable(false)]
+        public bool ValidFlag { get; set; }
+
+        /// <summary>
+        /// 資料版號
+        /// </summary>
+        [DisplayName("資料版號")]
+        [Editable(false)]
+        public byte[] Rowversion { get; set; }
+
     }
 }
